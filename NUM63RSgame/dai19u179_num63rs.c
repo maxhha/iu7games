41a84334c297ca07a481f69d00bf7490d0ae5d93
maxhha <devmaxhha@gmail.com>
2020-02-22 11:11:48 +0300

    Faster solve based on counting prs

--- NUM63RSgame/dai19u179_num63rs.c
+++ NUM63RSgame/dai19u179_num63rs.c
@@ -1,20 +1,43 @@
 #include <stdio.h>
+#define COUNTS_N 1000000
 
 int numbers_game(int min, int max)
 {
-    for(int i = max;;i++)
-    {
-        int passed = 1;
-        for(int j = min; j <= max && passed; j++)
-        {
-            if (i % j != 0)
-            {
-               passed = 0;
+    int counts[COUNTS_N];
+    
+    for(int i = 0; i < COUNTS_N; i++)
+        counts[i] = 0;
+     
+    int c;
+    for(int i = min; i <= max; i++) {
+        c = i;
+        int m = 0, j;
+        for(j = 2; j <= c;) {
+            if (c % j == 0) {
+                m += 1;
+                c /= j;
+            } else {
+                if (counts[j - 2] < m) {
+                    counts[j - 2] = m;
+                }
+                m = 0;
+                j++;
             }
         }
-        if (passed)
-        {
-            return i;
+        if (counts[j - 2] < m) {
+            counts[j - 2] = m;
         }
     }
+    int r = 1;
+    
+    for(int i = 0; i < COUNTS_N;)
+    {
+        if (counts[i] == 0) {
+            i++;
+        } else {
+            r *= i + 2;
+            counts[i] -= 1;
+        }
+    }
+    return r;
 }
