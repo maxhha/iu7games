04d825cc00a4cfee4bafee558c82afbaa34220dc
maxhha <devmaxhha@gmail.com>
2020-03-02 23:15:43 +0300

    Added cache for simple numbers

--- NUM63RSgame/dai19u179_num63rs.c
+++ NUM63RSgame/dai19u179_num63rs.c
@@ -1,42 +1,69 @@
 #include <stdio.h>
-#define COUNTS_N 1000000
+#define COUNTS_N 100
 
 int numbers_game(int min, int max)
 {
-    int counts[COUNTS_N];
-    
-    for(int i = 0; i < COUNTS_N; i++)
-        counts[i] = 0;
-     
-    int c;
-    for(int i = min; i <= max; i++) {
-        c = i;
-        int m = 0, j;
-        for(j = 2; j <= c;) {
-            if (c % j == 0) {
-                m += 1;
-                c /= j;
-            } else {
-                if (counts[j - 2] < m) {
-                    counts[j - 2] = m;
-                }
-                m = 0;
-                j++;
-            }
-        }
-        if (counts[j - 2] < m) {
-            counts[j - 2] = m;
-        }
+    int counts[COUNTS_N] = {0};
+    int simples[COUNTS_N] = {2, 3, 5, 7, 11, 13, 17, 19, 23};
+    int simples_n = 9;
+
+    if (min == 1) {
+      min = 2;
     }
-    int r = 1;
-    
-    for(int i = 0; i < COUNTS_N;)
+
+    int c, j, m, is_simple, k, z;
+    for(int i = max; i >= min; i--) {
+
+      c = i;
+      for(j = 0; j < simples_n && c > 1; j++) {
+        m = 0;
+        while(c % simples[j] == 0) {
+          c /= simples[j];
+          m++;
+        }
+
+        if (m > counts[j]) {
+          counts[j] = m;
+        }
+      }
+
+      for(j = simples_n; c > 1; j++) {
+        for(k = simples[j-1] + 1;;k++) {
+          is_simple = 1;
+          for(z = 0; z < j && is_simple; z++) {
+            is_simple = k % simples[z];
+          }
+          if (is_simple) {
+            simples[j] = k;
+            break;
+          }
+        }
+
+        m = 0;
+        while(c % simples[j] == 0) {
+          c /= simples[j];
+          m++;
+        }
+
+        if (m > counts[j]) {
+          counts[j] = m;
+        }
+      }
+
+      simples_n = j;
+    }
+
+    int x, p, r = 1;
+
+    for(int i = 0; i < simples_n; i++)
     {
-        if (counts[i] == 0) {
-            i++;
-        } else {
-            r *= i + 2;
-            counts[i] -= 1;
+        x = simples[i];
+        p = counts[i];
+        for(; p > 0; p >>= 1) {
+          if (p & 1) {
+            r *= x;
+          }
+          x *= x;
         }
     }
     return r;
